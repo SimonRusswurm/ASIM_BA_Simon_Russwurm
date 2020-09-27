d74bfa3ce28da04b02e7966982c225c9f5179fb7
SimonRusswurm <70999157+SimonRusswurm@users.noreply.github.com>
2020-09-27 20:58:58 +0200

    update sizing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var mc8 = document.querySelector(".mc8");
 var style = document.querySelector("style");
 var rom = document.querySelector(".ROM-Adresse");
+let wiH = window.innerHeight;
+let wiW = window.innerWidth;
 
 
 
@@ -13,21 +15,29 @@ for(var i = 0; i<224; i++){
     rom.appendChild(p);
 }
 
-
-
-
-window.addEventListener('resize', function () {
-    if(mc8.offsetWidth >= window.innerWidth){
-        mc8.style.width = "100vw";
-        mc8.style.height = "66.66666666667vw";
+window.addEventListener('DOMContentLoaded', function () {
+    if(window.innerHeight*1.5 > window.innerWidth){
+        mc8.style.width = String(window.innerWidth) + "px";
+        mc8.style.height = String(window.innerWidth/1.5) + "px";
         style.innerHTML = "h1{margin: 0; padding: 0; font-size: 1.46666vw; color: #19B2FF; font-family: lato-bold;} p{ margin: 0; padding: 0; font-size: 1.466666vw; color: #333333; font-family: lato-reg;}"
         
-    }
-   
-    if(mc8.offsetHeight >= window.innerHeight){        
-        mc8.style.width = "150vh";
-        mc8.style.height = "100vh";
+    } else{
+        mc8.style.width = String(window.innerHeight*1.5) + "px";
+        mc8.style.height = String(window.innerHeight) + "px";
         style.innerHTML = "h1{margin: 0; padding: 0; font-size: 2.2vh; color: #19B2FF; font-family: lato-bold;} p{ margin: 0; padding: 0; font-size: 2.2vh; color: #333333; font-family: lato-reg;}"
-    }
+    }  
+});
+
+window.addEventListener('resize', function () {
+    if(window.innerHeight*1.5 > window.innerWidth){
+        mc8.style.width = String(window.innerWidth) + "px";
+        mc8.style.height = String(window.innerWidth/1.5) + "px";
+        style.innerHTML = "h1{margin: 0; padding: 0; font-size: 1.46666vw; color: #19B2FF; font-family: lato-bold;} p{ margin: 0; padding: 0; font-size: 1.466666vw; color: #333333; font-family: lato-reg;}"
+        
+    } else{
+        mc8.style.width = String(window.innerHeight*1.5) + "px";
+        mc8.style.height = String(window.innerHeight) + "px";
+        style.innerHTML = "h1{margin: 0; padding: 0; font-size: 2.2vh; color: #19B2FF; font-family: lato-bold;} p{ margin: 0; padding: 0; font-size: 2.2vh; color: #333333; font-family: lato-reg;}"
+    }  
 });
 

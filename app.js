7c2d40b94783e663eb57d0b58b471a77ef70b32a
SimonRusswurm <70999157+SimonRusswurm@users.noreply.github.com>
2020-10-03 15:29:10 +0200

    design css/html update

--- app.js
+++ app.js
@@ -5,28 +5,28 @@ let style = document.querySelector("style");
 
 
 window.addEventListener('DOMContentLoaded', function () {
-    if(window.innerHeight*1.5 > window.innerWidth){
+    if(window.innerHeight*1.4375 > window.innerWidth){
         mc8.style.width = String(window.innerWidth) + "px";
-        mc8.style.height = String(window.innerWidth/1.5) + "px";
-        style.innerHTML = "h1{margin: 0; padding: 0; font-size: 1.46666vw; color: #19B2FF; font-family: lato-bold;} p{ margin: 0; padding: 0; font-size: 1.466666vw; color: #333333; font-family: lato-reg;}"
+        mc8.style.height = String(window.innerWidth/1.4375) + "px";
+        style.innerHTML = "h1{font-size: 1.46666vw;} p{font-size: 1.3913vw;} h2{font-size: 2.7vw;} h3{font-size: 1vw;}";
         
     } else{
-        mc8.style.width = String(window.innerHeight*1.5) + "px";
+        mc8.style.width = String(window.innerHeight*1.4375) + "px";
         mc8.style.height = String(window.innerHeight) + "px";
-        style.innerHTML = "h1{margin: 0; padding: 0; font-size: 2.2vh; color: #19B2FF; font-family: lato-bold;} p{ margin: 0; padding: 0; font-size: 2.2vh; color: #333333; font-family: lato-reg;}"
+        style.innerHTML = "h1{font-size: 2.2vh;} p{font-size: 2vh;} h2{font-size: 3.88125vh;} h3{font-size: 1.4375vh;}";
     }  
 });
 
 window.addEventListener('resize', function () {
-    if(window.innerHeight*1.5 > window.innerWidth){
+    if(window.innerHeight*1.4375 > window.innerWidth){
         mc8.style.width = String(window.innerWidth) + "px";
-        mc8.style.height = String(window.innerWidth/1.5) + "px";
-        style.innerHTML = "h1{margin: 0; padding: 0; font-size: 1.46666vw; color: #19B2FF; font-family: lato-bold;} p{ margin: 0; padding: 0; font-size: 1.466666vw; color: #333333; font-family: lato-reg;}"
+        mc8.style.height = String(window.innerWidth/1.4375) + "px";
+        style.innerHTML = "h1{font-size: 1.46666vw;} p{font-size: 1.3913vw;} h2{font-size: 2.7vw;} h3{font-size: 1vw;}";
         
     } else{
-        mc8.style.width = String(window.innerHeight*1.5) + "px";
+        mc8.style.width = String(window.innerHeight*1.4375) + "px";
         mc8.style.height = String(window.innerHeight) + "px";
-        style.innerHTML = "h1{margin: 0; padding: 0; font-size: 2.2vh; color: #19B2FF; font-family: lato-bold;} p{ margin: 0; padding: 0; font-size: 2.2vh; color: #333333; font-family: lato-reg;}"
+        style.innerHTML = "h1{font-size: 2.2vh;} p{font-size: 2vh;} h2{font-size: 3.88125vh;} h3{font-size: 1.4375vh;}";
     }  
 });
 }
@@ -34,17 +34,21 @@ window.addEventListener('resize', function () {
 
 //fill ROM
 {
-let rom = document.querySelector(".ROM-Adresse");
-let ram = document.querySelector(".RAM-Adresse");
+let rom = document.querySelector(".Adresse-000x-1FFx");
+let ram = document.getElementsByClassName("Adresse-200x-3FFx");
+console.log(ram);
 console.log(rom);
-
 for(var i = 0; i<224; i++){
     let ro = document.createElement('p');
     let ra = document.createElement('p');
     ro.textContent = "FF";
     ra.textContent = "FF";
     rom.appendChild(ro);
-    ram.appendChild(ra);
+    if(i<112){
+        ram[0].appendChild(ra);
+    }else{
+        ram[1].appendChild(ra);
+    }
 }
 }
 

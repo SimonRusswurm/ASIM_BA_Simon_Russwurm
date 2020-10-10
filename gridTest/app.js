0a974a8940fd7adfb8a2ebd643a885de274c719a
SimonRusswurm <70999157+SimonRusswurm@users.noreply.github.com>
2020-10-10 14:57:40 +0200

    big update

--- gridTest/app.js
+++ gridTest/app.js
@@ -8,12 +8,12 @@ window.addEventListener('DOMContentLoaded', function () {
     if(window.innerHeight*1.4375 > window.innerWidth){
         mc8.style.width = String(window.innerWidth) + "px";
         mc8.style.height = String(window.innerWidth/1.4375) + "px";
-        style.innerHTML = "h1{font-size: 1.46666vw;} p{font-size: 1.3913vw;} h2{font-size: 3vw;} .h2mov{font-size: 3vw;} h3{font-size: 1vw;} h4{font-size: 2.5vw}";
+        style.innerHTML = "h1{font-size: 1.46666vw;} p{font-size: 1.3913vw;} h2{font-size: 3vw;} h3{font-size: 1vw;} h4{font-size: 2.5vw}";
         
     } else{
         mc8.style.width = String(window.innerHeight*1.4375) + "px";
         mc8.style.height = String(window.innerHeight) + "px";
-        style.innerHTML = "h1{font-size: 2.2vh;} p{font-size: 2vh;} h2{font-size: 4.3125vh;} .h2mov{font-size: 4.3125vh;}h3{font-size: 1.4375vh;} h4{font-size: 3.59375vh}";
+        style.innerHTML = "h1{font-size: 2.2vh;} p{font-size: 2vh;} h2{font-size: 4.3125vh;} h3{font-size: 1.4375vh;} h4{font-size: 3.59375vh}";
     }  
 });
 
@@ -21,24 +21,40 @@ window.addEventListener('resize', function () {
     if(window.innerHeight*1.4375 > window.innerWidth){
         mc8.style.width = String(window.innerWidth) + "px";
         mc8.style.height = String(window.innerWidth/1.4375) + "px";
-        style.innerHTML = "h1{font-size: 1.46666vw;} p{font-size: 1.3913vw;} h2{font-size: 3vw;} .h2mov{font-size: 3vw;} h3{font-size: 1vw;} h4{font-size: 2.5vw}";
+        style.innerHTML = "h1{font-size: 1.46666vw;} p{font-size: 1.3913vw;} h2{font-size: 3vw;} h3{font-size: 1vw;} h4{font-size: 2.5vw}";
         
     } else{
         mc8.style.width = String(window.innerHeight*1.4375) + "px";
         mc8.style.height = String(window.innerHeight) + "px";
-        style.innerHTML = "h1{font-size: 2.2vh;} p{font-size: 2vh;} h2{font-size: 4.3125vh;} .h2mov{font-size: 4.3125vh; h3{font-size: 1.4375vh;} h4{font-size: 3.59375vh}";
+        style.innerHTML = "h1{font-size: 2.2vh;} p{font-size: 2vh;} h2{font-size: 4.3125vh;} h3{font-size: 1.4375vh;} h4{font-size: 3.59375vh}";
     }  
 });
 }
 
 
-
+// fill ROM
+{
+let rom = document.getElementsByClassName("Adresse-000x-1FFx");
+let ram = document.getElementsByClassName("Adresse-200x-3FFx");
+let j = 0;
+for(var i = 0; i<224; i++){
+    let ro = document.createElement('p');
+    let ra = document.createElement('p');
+    ro.textContent = "FF";
+    ra.textContent = "FF";
+    if(i%16 === 0 && i !== 0){
+        j++;
+    }
+    rom[j].appendChild(ro);
+    ram[j].appendChild(ra);
+}
+}
 
 
 //program
-let IO1 = document.getElementById('IO1');
-let I02 = document.getElementById('IO2');
-let I03 = document.getElementById('IO3');
+let IO1 = document.getElementById('io-input1');
+let I02 = document.getElementById('io-input2');
+let I03 = document.getElementById('io-input3');
 let A = document.getElementById('A');
 let c_flag = document.getElementById('c_flag');
 let z_flag = document.getElementById('z_flag');
@@ -62,7 +78,6 @@ let IO = document.getElementById('IO');
 let rom = document.querySelector(".Adresse-000x-1FFx");
 let ram = document.getElementsByClassName("Adresse-200x-3FFx");
 let romArray = ['3E','12','06','22'];
-let romPoints = [];
 let ramArray = [];
 
 const commands = [
@@ -114,103 +129,103 @@ const commands = [
 const fixPoints = [
     {
         x: 10,
-        y: 2,
+        y: 3,
         index: 0,
         label: "ROM1",
         parent: -1,
         childs: [1]
     },
     {
-        x: 14,
-        y: 2,
+        x: 15,
+        y: 3,
         index: 1,
         label: "",
         parent: 0,
         childs: [2,4]
     },
     {
-        x: 14,
-        y: 0,
+        x: 15,
+        y: 1,
         index: 2,
         label: "",
         parent: 2,
         childs: [3]
     },
     {
-        x: 16,
-        y: 0,
+        x: 17,
+        y: 1,
         index: 3,
         label: "IO1",
         parent: 2,
         childs: []
     },
     {
-        x: 18,
-        y: 2,
+        x: 19,
+        y: 3,
         index: 4,
         label: "",
         parent: 1,
         childs: [5,12]
     },
     {
-        x: 22,
-        y: 2,
+        x: 23,
+        y: 3,
         index: 5,
         label: "",
         parent: 4,
         childs: [6,8]
     },
     {
-        x: 22,
-        y: 0,
+        x: 23,
+        y: 1,
         index: 6,
         label: "",
         parent: 5,
         childs: [7]
     },
     {
-        x: 24,
-        y: 0,
+        x: 25,
+        y: 1,
         index: 7,
         label: "IO2",
         parent: 6,
         childs: []
     },
     {
-        x: 30,
-        y: 2,
+        x: 31,
+        y: 3,
         index: 8,
         label: "",
         parent: 5,
         childs: [9,11]
     },
     {
-        x: 30,
-        y: 0,
+        x: 31,
+        y: 1,
         index: 9,
         label: "",
         parent: 8,
         childs: [10]
     },
     {
-        x: 32,
-        y: 0,
+        x: 33,
+        y: 1,
         index: 10,
         label: "IO3",
         parent: 9,
         childs: []
     },
     {
-        x: 34,
-        y: 2,
+        x: 36,
+        y: 3,
         index: 11,
         label: "RAM1",
         parent: 8,
         childs: []
     },
     {
-        x: 18,
-        y: 4,
+        x: 19,
+        y: 5,
         index: 12,
         label: "",
         parent: 3,
@@ -218,7 +233,7 @@ const fixPoints = [
     },
     {
         x: 24,
-        y: 4,
+        y: 5,
         index: 13,
         label: "",
         parent: 12,
@@ -233,15 +248,15 @@ const fixPoints = [
         childs: []
     },
     {
-        x: 30,
-        y: 4,
+        x: 32,
+        y: 5,
         index: 15,
         label: "",
         parent: 13,
         childs: [16,17]
     },
     {
-        x: 30,
+        x: 32,
         y: 6,
         index: 16,
         label: "ALU2",
@@ -249,40 +264,40 @@ const fixPoints = [
         childs: []
     },
     {
-        x: 34,
-        y: 4,
+        x: 35,
+        y: 5,
         index: 17,
         label: "",
         parent: 15,
         childs: [18]
     },
     {
-        x: 34,
-        y: 12,
+        x: 35,
+        y: 13,
         index: 18,
         label: "",
         parent: 17,
         childs: [19,21]
     },
     {
-        x: 27,
-        y: 12,
+        x: 28,
+        y: 13,
         index: 19,
         label: "",
         parent: 18,
         childs: [20]
     },
     {
-        x: 27,
-        y: 10,
+        x: 28,
+        y: 12,
         index: 20,
         label: "ALUOUT",
         parent: 19,
         childs: []
     },
     {
-        x: 34,
-        y: 14,
+        x: 35,
+        y: 15,
         index: 21,
         label: "",
         parent: 18,
@@ -290,181 +305,150 @@ const fixPoints = [
     },
     {
         x: 32,
-        y: 14,
+        y: 15,
         index: 22,
         label: "SW",
         parent: 21,
         childs: []
     },
     {
-        x: 13,
-        y: 4,
+        x: 14,
+        y: 5,
         index: 23,
         label: "",
         parent: 12,
         childs: [24,25]
     },
     {
-        x: 13,
-        y: 6,
+        x: 14,
+        y: 7,
         index: 24,
         label: "A",
         parent: 23,
         childs: []
     },
     {
-        x: 10,
-        y: 4,
+        x: 11,
+        y: 5,
         index: 25,
         label: "",
         parent: 23,
         childs: [26]
     },
     {
-        x: 10,
-        y: 15,
+        x: 11,
+        y: 16,
         index: 26,
         label: "",
         parent: 25,
         childs: [27]
     },
     {
-        x: 14,
-        y: 15,
+        x: 16,
+        y: 16,
         index: 27,
         label: "",
         parent: 26,
-        childs: [28,33]
+        childs: [28,32]
     },
     {
-        x: 14,
-        y: 14,
+        x: 16,
+        y: 15,
         index: 28,
         label: "IX",
         parent: 27,
-        childs: [29]
+        childs: [28]
     },
     {
-        x: 14,
-        y: 12,
+        x: 16,
+        y: 13,
         index: 29,
         label: "HL",
         parent: 28,
         childs: [30]
     },
     {
-        x: 14,
-        y: 10,
+        x: 16,
+        y: 11,
         index: 30,
-        label: "",
+        label: "C",
         parent: 29,
-        childs: [31,32]
-
+        childs: [31]
     },
     {
-        x: 13,
-        y: 10,
+        x: 14,
+        y: 11,
         index: 31,
         label: "B",
-        parent: 30,
-        childs: [32]
-    },
-    {
-        x: 15,
-        y: 10,
-        index: 32,
-        label: "C",
         parent: 30,
         childs: []
     },
     {
-        x: 14,
-        y: 16,
-        index: 33,
+        x: 16,
+        y: 17,
+        index: 32,
         label: "SP",
         parent: 27,
+        childs: [33]
+    },
+    {
+        x: 16,
+        y: 19,
+        index: 33,
+        label: "PC",
+        parent: 32,
         childs: [34]
     },
     {
-        x: 14,
-        y: 18,
+        x: 16,
+        y: 21,
         index: 34,
-        label: "PC",
+        label: "ZR",
         parent: 33,
         childs: [35]
     },
     {
-        x: 14,
-        y: 20,
+        x: 16,
+        y: 25,
         index: 35,
-        label: "ZR",
-        parent: 34,
-        childs: [36]
-    },
-    {
-        x: 14,
-        y: 24,
-        index: 36,
         label: "",
-        parent: 35,
-        childs: [37,38]
+        parent: 34,
+        childs: [36,37]
     },
     {
         x: 10,
-        y: 24,
-        index: 37,
+        y: 25,
+        index: 36,
         label: "ROM2",
-        parent: 36,
+        parent: 35,
         childs: []
     },
     {
-        x: 28,
-        y: 24,
-        index: 38,
+        x: 29,
+        y: 25,
+        index: 37,
         label: "",
-        parent: 36,
-        childs: [39,40]
+        parent: 35,
+        childs: [38,39]
     },
     {
-        x: 28,
+        x: 29,
         y: 26,
-        index: 39,
+        index: 38,
         label: "DEC",
-        parent: 38,
+        parent: 37,
         childs: []
     },
     {
-        x: 34,
-        y: 24,
-        index: 40,
+        x: 36,
+        y: 25,
+        index: 39,
         label: "RAM2",
-        parent: 38,
+        parent: 37,
         childs: []
-    },
+    }
 ]
 
-
-// fill ROM
-{
-    let j = 0;
-    for(var i = 0; i<224; i++){
-        let romElement = document.createElement('p');
-        romElement.classList.add('romElement', 'grid-template');
-        romElement.id = "romElement" + String(i);
-        if(!(i%8) && i !== 0)
-            j++;
-        
-       if(i<romArray.length){
-           romElement.textContent = romArray[i]
-       } else {
-            romElement.textContent ='FF';
-       }
-
-        romElement.style.top = String(100/32*(j+2)) + "%";
-        romElement.style.left = String(100/46*((i%8)+2)) + "%";
-        document.querySelector(".gridcontainer").appendChild(romElement);    
-    }
-}
 
 //-----------------------------------Test starts here--------------------------
 
@@ -478,27 +462,62 @@ const FRAMERATE = 50;
 let OFFSET_X = 0;
 let OFFSET_Y = 0;
 
+// for(let i = 0; i<100; i++){
+//     let p = document.createElement('p');
+//     p.innerHTML = String(i);
+//     p.id = String(i);
+//     document.querySelector(".grid").appendChild(p);
+// }
 
-function createMovingObj(elementId, aPath){
-    element = document.getElementById(elementId);
+
+function createMovingObj(element, aPath){
     let clone = element.cloneNode(true);
-    clone.classList.add("boxborder", "rounded");
-    clone.style.zIndex = "10";
     clone.id = "clonedElement";
     clone.style.background = "yellow";
-    clone.style.top = String(100/32*aPath[0].y) +"%";
-    clone.style.left = String(100/46*aPath[0].x) +"%";
+    clone.style.height = String(100/32*2) + "%";
+    clone.style.width = String(100/46*2) + "%";
+    clone.style.borderRadius = "0.5vw";
+    clone.style.position = "absolute";
+    clone.style.top = String(100/32*aPath[0].y) + "%";
+    clone.style.left = String(100/46*aPath[0].x) + "%";
 
-
-    document.querySelector(".gridcontainer").appendChild(clone);
+    document.querySelector(".globalgrid").appendChild(clone);
     let  movObj = {aDiv: clone, path: aPath};
     return movObj;
 
+
+
+
+    // let iDiv = document.createElement('h4');
+    // let h = 3.125*1.6;
+    // let w = 3.125/1.4375*1.6;
+    
+    // OFFSET_X = w/2;
+    // OFFSET_Y = h/2;
+    // iDiv.id = 'movObj';
+    // iDiv.className = 'movObj';
+    // iDiv.style.position = "absolute";
+    // iDiv.style.top = String(aPath[0].y*3.125-OFFSET_Y) + "%" ;
+    // iDiv.style.left = String(aPath[0].x*3.125/1.4375-OFFSET_X) + "%";
+    // iDiv.style.display = "grid";
+    // iDiv.style.justifyContent = "center";
+    // iDiv.style.alignItems = "center";
+    // iDiv.style.height = String(h) + "%";
+    // iDiv.style.width = String(w) +"%";
+    // iDiv.innerHTML = "FF";
+    // iDiv.style.background = "yellow";
+    // iDiv.style.zIndex = "10";
+    // iDiv.style.border = "0.5px solid #666666"
+    // iDiv.style.borderRadius = "0.5vw";
+    // document.querySelector(".globalgrid").appendChild(iDiv);
+
+    // let movObj = {aDiv: iDiv, path: aPath};
+    // return movObj;
 }
 
 //AtoB
 {
-//returns index array 0 to A
+
 function getPathZerotoA(pointIndexA){
     let zeroToPointA = [0];
     let loop = true;
@@ -575,25 +594,16 @@ function getPathAtoB(pointIndexA, pointIndexB){
     for(let i=0; i < AtoB.length; i++){
         AtoB[i] = fixPoints[AtoB[i]];
     }
+
+    console.log(AtoB);
     return AtoB;
 }
 
 //returns an array of points
-function pathFromTo(pointLabelA, pointLabelB){
-    let pointA = 0;
-    let pointB = convertlabelToPoint(pointLabelB);
+function pathFromTo(pointA, pointB){
     let fixPointIndexA = -1;
     let fixPointIndexB = -1;
     let AtoB = [];
-
-    if(pointLabelA.includes('romElement')){
-        AtoB = pathFromTo("ROM1", pointLabelB);
-        AtoB = romElementToROM1(pointLabelA).concat(AtoB);
-        return AtoB;
-
-    } else {
-        pointA = convertlabelToPoint(pointLabelA);
-    }
     
     for(let i = 0; i<fixPoints.length;i++){
         if(pointA.x === fixPoints[i].x && pointA.y === fixPoints[i].y){
@@ -627,33 +637,11 @@ function convertlabelToPoint(label){
     }
     return point;
 }
-function romElementToROM1(romElementID){
-    let toROM1 = [];
-    let romElement = document.getElementById(romElementID);
-    let rEx = romElement.style.left.replace('%','');
-    let rEy = romElement.style.top.replace('%','');
-    rEx = Math.round(Number(rEx) *46/100);
-    rEy = Math.round(Number(rEy)*32/100);
-    let romBetweenPoint = {
-        x: rEx,
-        y: 2
-    };
-    let romPoint = {
-        x: rEx,
-        y: rEy,
-    };
-    toROM1.push(romPoint);
-    toROM1.push(romBetweenPoint);
-    return toROM1;
-}
 }
 
-
-
-
-let path = pathFromTo("romElement130","SW");
-
-let mov = createMovingObj('romElement130', path);
+let path = pathFromTo(convertlabelToPoint("A"), convertlabelToPoint("SW"));
+console.log(path);
+let mov = createMovingObj(A, path);
 
 let j = 0;
 
@@ -674,22 +662,14 @@ function updateMovObjPosition(aMovObj){
         } else if(aMovObj.path[1].y <  aMovObj.path[0].y){
             aMovObj.path[0].y -= ANIMATION_SPEED;
             aMovObj.path[0].y = Math.round(aMovObj.path[0].y*100)/100;
-        }
-        
-
-        //if RomElement
-        if(Math.floor(aMovObj.path[0].x) === 9 && Math.floor(aMovObj.path[0].y) === 2){
-            aMovObj.aDiv.classList.add('square2x2' , 'h2mov');   
-        }
+        }        
        
         if(Math.abs(aMovObj.path[1].x-aMovObj.path[0].x) < ANIMATION_SPEED && Math.abs(aMovObj.path[1].y - aMovObj.path[0].y) < ANIMATION_SPEED){
             aMovObj.path.shift();
         }
-        aMovObj.aDiv.style.top = String(aMovObj.path[0].y*100/32) + "%" ;
-        aMovObj.aDiv.style.left = String(aMovObj.path[0].x*100/46) + "%";
-    }else{
-        aMovObj.aDiv.remove();
-    }
+        aMovObj.aDiv.style.top = String((aMovObj.path[0].y-1)*100/32) + "%" ;
+        aMovObj.aDiv.style.left = String((aMovObj.path[0].x-1)*100/46) + "%";
+    } 
 }
 
 function main(currentTime){
